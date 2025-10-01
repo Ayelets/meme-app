014f1899f2291def3c6d289ad60acc1120c58d48
Ayelets <ayelet126@gmail.com>
2025-10-02 00:28:14 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -569,13 +569,13 @@ useEffect(() => {
             onPointerUp={onPointerUp}
             onPointerLeave={onPointerUp}
             className="relative w-full bg-zinc-100 rounded-xl overflow-hidden border touch-pan-y  mx-auto"
-            style={{ aspectRatio: aspect}}
+            style={{ aspectRatio: aspect, maxHeight: 'min(85dvh, calc(100dvh - 220px))' }}
           >
           <img
             src={activeUrl}
             alt="active"
             className="absolute inset-0 h-full w-full object-contain select-none pointer-events-none"
-            style={{ objectPosition: 'center center' }}
+            style={{ objectPosition: 'center' }}
           />
 
             {boxes.map((b) => (
